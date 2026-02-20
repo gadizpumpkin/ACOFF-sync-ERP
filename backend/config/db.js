1b1c85deaab0f6e8bf8636437ddfa1adc7468768
GadizPumpkin <sepiananda99@gmail.com>
2026-02-20 22:40:43 +0700

    update connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,10 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "coffee_street"
 });
 
-module.exports = db;
+module.exports = db.promise();
