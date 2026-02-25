37a53b12f874b139a33b70d026dd0e54654560a1
GadizPumpkin <sepiananda99@gmail.com>
2026-02-25 21:41:53 +0700

    perbaikan koneksi di serverjs & dbjs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "coffee_street"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = db.promise();
--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/transaksi", require("./routes/transaksiRoutes"));
-app.use("/api/bahan", require("./routes/bahanRoutes"));
-app.use("/api/pembelian", require("./routes/pembelianRoutes"));
-app.use("/api/payroll", require("./routes/payrollRoutes"));
-app.use("/api/laporan", require("./routes/laporanRoutes"));
-app.use("/api/audit", require("./routes/auditRoutes"));
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
