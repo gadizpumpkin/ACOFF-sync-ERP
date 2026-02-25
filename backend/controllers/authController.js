7a0ef7938f8a307c0c9db4803d3fa39332114079
GadizPumpkin <sepiananda99@gmail.com>
2026-02-25 21:42:57 +0700

    perbaikan authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -25,7 +25,7 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign(
       { id: user.id, role: user.role },
-      "SECRET_KEY",
+      process.env.JWT_SECRET,
       { expiresIn: "8h" }
     );
 
